d8a9c2594344aec0ff893292b8d929a8b663ee3f
Roberto <roberto.scinocca@gmail.com>
2015-02-09 22:42:17 +0100

    FEATURE!

--- src/part/Quadrato.java
+++ src/part/Quadrato.java
@@ -18,29 +18,43 @@ public class Quadrato {
     double speed;
     private Boolean path;
     
+    int timer;
+    
     public Quadrato(double pSpeed) {
         this.square = new ImageIcon(getClass().getResource("/immagini/square.png")).getImage();
         this.y = Math.random() * (600 - this.square.getHeight(null));
-        this.x = 800 - this.square.getWidth(null);
+        this.x = 800;//- this.square.getWidth(null);
         this.width = this.square.getWidth(null);
         this.heigth = this.square.getHeight(null);
         this.speed = pSpeed;
         this.path = false;
         this.Body = new Rectangle((int)this.x, (int)this.y, (int)this.width, (int)this.heigth );
+    
+        this.timer = (int)(Math.random() * 250);
     }
     
     public void update() {
-        if(!path) //fa andare in un senso on in un altro il quadrato
-            this.x -= this.speed;
-        else 
-            this.x += this.speed;
-        
-        if(this.x <= 0 - this.square.getWidth(null) || this.x >= 800){
-            this.x = 800;
-            this.y = (Math.random() * (600 - square.getHeight(null)));
-            this.path = false;
+        if(this.ready_to_spawn()) {
+            if(!path) //fa andare in un senso on in un altro il quadrato
+                this.x -= this.speed;
+            else 
+                this.x += this.speed;
+
+            if(this.x <= 0 - this.square.getWidth(null) || this.x >= 800){
+                this.x = 800;
+                this.y = (Math.random() * (600 - square.getHeight(null)));
+                this.path = false;
+            }
+            this.Body = new Rectangle((int)this.x, (int)this.y, (int)this.width, (int)this.heigth );
         }
-        this.Body = new Rectangle((int)this.x, (int)this.y, (int)this.width, (int)this.heigth );
+    }
+    
+    public boolean ready_to_spawn() {
+        if(this.timer == 0)
+            return true;
+        else
+            this.timer--;
+        return false;
     }
     
     public Rectangle getBody() {
